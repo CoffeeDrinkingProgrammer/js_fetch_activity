5ba6f722e76a443a088038c3ec591d18fca35196
CoffeeDrinkingProgrammer <paulo.ata88@gmail.com>
2021-11-14 14:24:30 +0800

    create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react';
 
 function App() {
+  const [quote, setQuote] = useState(0);
+  const getQuote = () => {
+    fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      setQuote(response)
+    })
+  }
+
+  useEffect(() => {
+    fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      setQuote(response)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <header> Paulo's React JS with API</header>
+      <div className="content">
+        Random Quote to Brighten up your day
+        {(quote.statusCode !== 404)? 
+          <center>
+              <div className="quote">{quote.content}</div>
+              <div className="author"> - {quote.author} </div>
+            
+          </center>
+        : <i> nothing to see here </i>
+        }
+      </div>
+      <button onClick={getQuote}> Try Again! </button>
     </div>
   );
 }
